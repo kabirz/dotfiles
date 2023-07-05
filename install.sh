65f4e9beffc729215eca8d53043e303c907a9eba
zhp <jxwazxzhp@126.com>
2023-07-05 14:03:09 +0800

    add nvim zimfw starship to dotfiles
    Signed-off-by: zhp <jxwazxzhp@126.com>

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# enter bash
+# source thisfile
+# install_xxx
+
+# install nvim
+install_nvim() {
+  pushd NvChad || exit
+  ln -s "$(pwd)"/custom nvim/lua/custom
+  ln -s "$(pwd)"/nvim ~/.config/nvim
+  popd || exit
+}
+
+# install tmux
+install_tmux() {
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  ln -s "$(pwd)"/tmux.conf ~/.tmux.conf
+}
+
+# install cargo_config
+install_cargo_config() {
+  ln -s "$(pwd)"/cargo_config.toml ~/.cargo/config
+}
+
+# install alacritty
+install_alacritty() {
+  ln -s "$(pwd)"/alacritty ~/.config/alacritty
+}
+
+# install starship
+install_starship() {
+  ln -s "$(pwd)"/starship.toml ~/.config/starship.toml
+}
